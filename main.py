0e57f1297762b75cb767ec9f91ab34fe2838aa45
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-10-05 16:39:46 +0100

    add Query type with movies field and register schema

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ class Movie:
     title: str
     year: int
     rating: float
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def movies(self) -> list[Movie]:
+        return [
+            Movie(id=1, title="Inception", year=2010, rating=4.8),
+            Movie(id=2, title="Interstellar", year=2014, rating=4.6)
+        ]
+
+
+schema = strawberry.Schema(query=Query)
