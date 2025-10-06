37ab798f5bae7d40a3f892dad1deda8b6871b35b
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-10-06 16:59:29 +0100

    set up FastAPI app and mount GraphQL route at /graphql

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import strawberry
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
 
+# GraphQL type definition for Movie
+# This describes the shape of movie data in our API.
 @strawberry.type
 class Movie:
     id: int
@@ -10,6 +13,11 @@ class Movie:
     rating: float
 
 
+# GraphQL Query type
+# Acts as the entry point for clients to fetch data.
+# The "movies" field is a resolver function:
+# - It returns a list of Movie objects (currently hardcoded for testing).
+# - In a real app, this would fetch data from a database.
 @strawberry.type
 class Query:
     @strawberry.field
@@ -20,4 +28,18 @@ class Query:
         ]
 
 
+# Create the GraphQL schema object
+# This registers the Query type as the root for all queries.
+# The schema ties together our type definitions (Movie) and resolvers (movies).
 schema = strawberry.Schema(query=Query)
+
+
+app = FastAPI()
+
+# Turn the schema into a GraphQL route
+graphql_app = GraphQLRouter(schema)
+
+
+# Mount the GraphQL route at /graphql
+app.include_router(graphql_app, prefix="/graphql")
+
